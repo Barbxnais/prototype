3adb5360157e174e5d3010d6687539151483b7ea
barbxnais <barnnaiss7.6@gmail.com>
2025-10-12 21:22:39 -0300

    Agregué invitados, solucioné mapa, decoracion y otros

--- src/App.jsx
+++ src/App.jsx
@@ -29,8 +29,29 @@ import decofinal from "./assets/decofinal.png"
 
 
 const codigosValidos = {
-  FAM01: { nombre: "Familia Méndez De La Fuente", invitados: 3 },
+  FAM01: { nombre: "Familia De La Fuente Riquelme", invitados: 2 },
   FAM02: { nombre: "Familia Fernández De La Fuente", invitados: 4 },
+  FAM03: { nombre: "Familia Méndez De La Fuente", invitados: 3},
+  FAM04: { nombre: "Familia Méndez Aedo", invitados: 3},
+  FAM05: { nombre: "Familia Mendez Muñoz", invitados: 3},
+  FAM06: { nombre: "Familia Mundaca De La Fuente", invitados: 3},
+  FAM07: { nombre: "Familia Acuña De La Fuente", invitados: 5},
+  FAM08: { nombre: "Familia Morales Riquelme", invitados: 2},
+  FAM09: { nombre: "Familia Betancourt Riquelme", invitados: 2},
+  FAM10: { nombre: "Familia Castillo Riquelme", invitados: 2},
+  FAM11: { nombre: "Familia Olavarría Jofré", invitados: 2},
+  FAM12: { nombre: "Familia Varas Jofré", invitados: 2},
+  FAM13: { nombre: "Familia Martínez Delgado", invitados: 2},
+  FAM14: { nombre: "Familia ??Treufu", invitados: 1},
+  FAM15: { nombre: "Familia Sotomayor", invitados: 2},
+  FAM16: { nombre: "Familia jairo y novia???", invitados: 2},
+  FAM17: { nombre: "Familia Aguilar???", invitados: 1},
+  FAM18: { nombre: "Familia Bahamondes Aravena", invitados: 4},
+  FAM19: { nombre: "Familia Bahamondes Ramírez", invitados: 2},
+  FAM20: { nombre: "Familia Maquehue Mujica", invitados: 2},
+  FAM21: { nombre: "Familia Castro Mendiguren", invitados: 2},
+  FAM22: { nombre: "Elevyn Ruiz y Raúl ??", invitados: 2},
+  FAM23: { nombre: "Francisca Carrillo ", invitados: 1},
 };
 
 function App() {
@@ -141,7 +162,7 @@ function App() {
 
             <div className="seccion">
               <h1>¡Estás Invitado!</h1>
-              <p>
+              <p className="parrafo-montserrat">
                 Nos encantaría que seas parte de este momento tan especial para
                 nosotros.
               </p>
@@ -175,7 +196,7 @@ function App() {
               <p><strong>Ver aquí:</strong></p>
               <div className="mapa-container">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=..."
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d745.5830640659888!2d-71.81517183302694!3d-36.35164805255087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x966f31003ceeaf25%3A0xc4d8cccecda72365!2sla%20posada%20de%20burgos!5e1!3m2!1ses-419!2scl!4v1760303316092!5m2!1ses-419!2scl"
                   width="100%"
                   height="300"
                   style={{ border: 0 }}
@@ -270,7 +291,8 @@ function App() {
             </div>
 
             <div className="seccion confirmar">
-              <p>Por favor confirma tu asistencia antes del 30 de noviembre.</p>
+              <p className="parrafo-montserrat">
+                Por favor confirma tu asistencia antes del 30 de noviembre.</p>
               <a
                 href="https://forms.office.com/Pages/ResponsePage.aspx?id=oeChOLG26UKzqVl2JmcLF_JwtzCeoyRDunc9P2IxQV1UQVEyOTkyQ0xOR1NEUlM4RUg1NlgwUVQ3Wi4u"
                 target="_blank"
@@ -291,7 +313,7 @@ function App() {
                 navigation
                 pagination={{ clickable: true }}
                 className="mySwiper"
-                style={{ width: "100%", maxWidth: "1000px", margin: "0 auto", height: "32rem" }} // altura fija del carrusel
+                style={{ width: "100%", maxWidth: "1000px", margin: "0 auto", height: "32rem" }} 
               >
                 {[
                   foto1, foto2, foto3, foto4,
@@ -308,7 +330,7 @@ function App() {
                         width: "100%",
                         height: "100%",
                         overflow: "hidden",
-                        background: "transparent", // opcional: #f7f7f7 para ver el área
+                        background: "transparent", // opcional: #f7f7f7 para ver el á
                         borderRadius: 12,
                       }}
                     >
@@ -331,10 +353,14 @@ function App() {
    
             <div className="seccion final">
               <p>¡Gracias por ser parte de este momento tan especial!</p>
-              <img
+              < motion.img
               src={decofinal}
               alt="deco final"
-              className="mx-auto mt-4 max-w-xs opacity-90"
+              className="mx-auto max-w-xs opacity-90 block"
+              initial={{ opacity: 0, y: 30 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1.2, ease: "easeOut" }}
+              viewport={{ once: true }}
               />
             </div>
           </section>
